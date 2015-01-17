05e99c3158574362c97b652f094cc270d6d9ba33
Alok Tripathy <kullalok@gmail.com>
2015-01-17 14:08:17 -0500

    Fixed the design move

--- server/app.js
+++ server/app.js
@@ -2,12 +2,14 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+//var python = require('node-python');
 
-app.use(express.static(__dirname + '/views/design'));
+app.use(express.static(__dirname + '/views'));
 
 io.on('connection', function(socket) {
-  console.log('a user has connected');
+  
 });
+
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
