e83df829e3a884f3a825a0f0069d001b6c5815ef
Alok Tripathy <kullalok@gmail.com>
2015-01-17 15:23:04 -0500

    Added python module again. Sorry Ben.

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var python = require('node-python');
 
 app.use(express.static(__dirname + '/views'));
 
@@ -21,3 +22,6 @@ io.on('connection', function(socket) {
 http.listen(3000, function() {
   console.log('listening on *:3000');
 });
+
+// interop with the python program
+var image_search = python.import('image_search.image_search');
