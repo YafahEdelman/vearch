29a6c6356f070f62493e42ac7b66d6e88767f5a0
Alok Tripathy <kullalok@gmail.com>
2015-01-17 15:19:22 -0500

    Minor change to accepting user input

--- server/app.js
+++ server/app.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var python = require('node-python');
 
 app.use(express.static(__dirname + '/views'));
 
@@ -22,6 +21,3 @@ io.on('connection', function(socket) {
 http.listen(3000, function() {
   console.log('listening on *:3000');
 });
-
-//interop with the python program
-var image_search = python.import('image_search.image_search');
