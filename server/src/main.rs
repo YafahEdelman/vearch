b69648f5749cc0fddc42a44e6dfe5b082f670ed7
Aaron Weiss <aaronweiss74@gmail.com>
2015-01-16 22:38:57 -0500

    Added dependencies.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,12 @@
+extern crate avcodec55;
+extern crate avformat55;
+extern crate hyper;
+extern crate image;
+extern crate iron;
+extern crate router;
+
+mod process;
+
 fn main() {
     println!("Hello, world!");
 }
