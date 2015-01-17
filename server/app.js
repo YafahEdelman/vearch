64b90da446d36150d12e39430253e7b12cf7ef87
Alok Tripathy <kullalok@gmail.com>
2015-01-17 13:51:41 -0500

    Added serving css

--- server/app.js
+++ server/app.js
@@ -1,13 +1,12 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/views/index.html');
-});
+app.use(express.static(__dirname + '/views/design'));
 
-io.on('connection', function(socket){
-  console.log('a user connected');
+io.on('connection', function(socket) {
+  console.log('a user has connected');
 });
 
 http.listen(3000, function(){
