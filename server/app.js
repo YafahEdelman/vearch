2ede7ac41c7fa1ae606aef6b0d29d2e098a70835
Alok Tripathy <kullalok@gmail.com>
2015-01-17 15:15:16 -0500

    Added accepting the YouTube link and search terms

--- server/app.js
+++ server/app.js
@@ -7,7 +7,15 @@ var io = require('socket.io')(http);
 app.use(express.static(__dirname + '/views'));
 
 io.on('connection', function(socket) {
-  
+  console.log("a user has joined");
+  socket.on("link submitted", function(link, terms) {
+    if (link == "" || terms == "") {
+      io.emit("missing link or terms");
+      return;
+    }
+    console.log("Whee a link " + link);
+    console.log("Whee some terms " + terms);
+  });
 });
 
 
