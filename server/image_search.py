eeb4f2534a1a9b4022ba9e258c09bcec34dfa99d
JacobEdelman <edelman.jd@gmail.com>
2015-01-18 00:22:43 -0500

    improved parts

--- server/image_search.py
+++ server/image_search.py
@@ -1,23 +1,28 @@
 from numpy import load
 
 from sys import path
-caffe_root = "../caffe"
-path.insert(0, caffe_root + '/python')
-from caffe import Classifier, set_mode_cpu, set_mode_gpu
-from caffe.io import load_image
+try:
+    from caffe import Classifier, set_mode_cpu, set_mode_gpu
+    from caffe.io import load_image
+except:
+    caffe_root = "../caffe"
+    path.insert(0,  '../caffe/python')
+    from caffe import Classifier, set_mode_cpu, set_mode_gpu
+    from caffe.io import load_image
+
 from os import listdir
 from os.path import join as join_paths
-word_data_file_name = caffe_root + "/data/ilsvrc12/synset_words.txt"
+word_data_file_name =  "../data/synset_words.txt"
 word_data_file = open(word_data_file_name)
 word_data = []
 for i in word_data_file.read().split("\n"):
     if " " in i:
         word_data.append(i[i.index(" ") + 1:])
 word_data_file.close()
-MODEL_FILE = caffe_root + '/models/bvlc_reference_caffenet/deploy.prototxt'
-PRETRAINED = caffe_root + '/models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel'
+MODEL_FILE =  '../data/deploy.prototxt'
+PRETRAINED =  '../data/bvlc_reference_caffenet.caffemodel'
 net = Classifier(MODEL_FILE, PRETRAINED,
-                       mean = load(caffe_root + '/python/caffe/imagenet/ilsvrc_2012_mean.npy'),
+                       mean = load('../data/ilsvrc_2012_mean.npy'),
                        channel_swap = (2, 1, 0),
                        raw_scale = 255,
                        image_dims = (256, 256))
